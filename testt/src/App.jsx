141c44926b9fa58e112ea74bbf32b2b8b35f7cd7
The1976 <89992861+The1976@users.noreply.github.com>
2025-08-12 16:30:31 +0300

    asdasd

---
+++ testt/src/App.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./components/Navbar.jsx";
+import Listeler from "./components/Liste.jsx";
+export default function App() {
+    return (
+        <>
+            <Navbar />
+            <div className='container'>
+            </div>
+        </>
+    )
+}
---
+++ testt/src/components/Liste.jsx
@@ -0,0 +1,11 @@
+export default function Listeler() {
+    return (
+        <>
+            <li>Domates</li>
+            <li>Biber</li>
+            <li>Patlican</li>
+            <li>Pazartesi</li>
+            <li>Sali</li>
+        </>
+    )
+}
